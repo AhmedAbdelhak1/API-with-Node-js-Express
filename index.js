7837f3c2562bd7a7efc98cf87bc11c32bc3675ac
AhmedAbdelhak1 <ahmedabdelhak0@gmail.com>
2020-10-12 23:25:29 +0200

    add POST req

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+const Joi = require('joi');
 const express = require('express');
 const app = express();
+
+app.use(express.json());
 
 const courses = [
     {id:1, name: 'Electronic'},
@@ -7,6 +10,27 @@ const courses = [
     {id:3, name: 'machine learning'},
     {id:4, name: 'Deep learning'}
 ];
+
+app.post('/api/courses',(req, res)=>{
+    const schema ={
+        name: Joi.string().min(3).required()
+    };
+
+    const result = Joi.validate(req.body, schema);
+    
+
+
+    if(result.error){
+        res.status(400).send(result.error);
+        return;
+    }
+    const course ={
+        id: courses.length +1,
+        name: req.body.name
+    }
+    courses.push(course);
+    res.send(course);
+});
 
 app.get('/',(req,res)=>{
     res.send("Hi what's up");
@@ -20,6 +44,7 @@ app.get('/api/courses/:id',(req,res)=>{
     if (!course) res.status(404).send('the course with this ID do not exist!!')
     res.send(course);
 });
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, ()=> console.log(`Listening on port ${port}....!`))
