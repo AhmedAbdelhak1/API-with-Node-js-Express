da2d21a8d510a1e794538afbb6f81082ac62763b
AhmedAbdelhak1 <ahmedabdelhak0@gmail.com>
2020-10-12 21:13:34 +0200

    another changes

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const app = express();
 
+const courses = [
+    {id:1, name: 'Electronic'},
+    {id:2, name: 'data stracture'},
+    {id:3, name: 'machine learning'},
+    {id:4, name: 'Deep learning'}
+];
+
 app.get('/',(req,res)=>{
     res.send("Hi what's up");
 });
 
 app.get('/api/courses',(req,res)=>{
-    res.send([1, 2, 3, 4]);
+    res.send(courses);
+});
+app.get('/api/courses/:id',(req,res)=>{
+    const course = courses.find(c=> c.id === parseInt(req.params.id));
+    if (!course) res.status(404).send('the course with this ID do not exist!!')
+    res.send(course);
 });
 
-
-app.listen('3000', ()=> console.log('Listening on port 3000....!'))
+const port = process.env.PORT || 3000;
+app.listen(port, ()=> console.log(`Listening on port ${port}....!`))
